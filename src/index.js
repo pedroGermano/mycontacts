f10041f27cd5d440b11615e36caf577d9ec204a3
Pedro Germano <pedrogermano232@gmail.com>
2023-06-08 21:06:17 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const app = express()
 
 app.get('/teste', (request, response) => {
   response.send('Hello World')
+  response.send('Hello World')
 });
+
 
 app.listen(3000, () => console.log('🔥 Server stardet at http://localhost:3000'));
