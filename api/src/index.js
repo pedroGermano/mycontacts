cee5d6f06289fe5044334034cde0de7b5abb7a99
Pedro Germano <pedrogermano232@gmail.com>
2023-06-13 17:09:26 -0300

    Middleware

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+app.use((request, response) => {
+
+});
+
 app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server stardet at http://localhost:3000'));
