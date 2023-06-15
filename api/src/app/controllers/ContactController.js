110cefa0ed1f9c4a60aa46bfb70315b7c6224458
Pedro Germano <pedrogermano232@gmail.com>
2023-06-14 22:45:15 -0300

    Name validation

--- api/src/app/controllers/ContactController.js
+++ api/src/app/controllers/ContactController.js
@@ -23,7 +23,11 @@ class ContactController {
       name, email, phone, category_id,
     } = request.body;
 
-    const contactExists = await ContactsRepository.findByIdEmail(email);
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
       return response.status(400).json({ error: 'This e-mail is already been taken' });
