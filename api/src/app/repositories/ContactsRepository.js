92743d233f8099f46fe127b56a000799e722eaad
Pedro Germano <pedrogermano232@gmail.com>
2023-06-23 12:43:51 -0300

    ordem

--- api/src/app/repositories/ContactsRepository.js
+++ api/src/app/repositories/ContactsRepository.js
@@ -21,20 +21,18 @@ let contacts = [
 
 class ContactsRepository {
   async findAll() {
-    const rows = await db.query('SELECT * FROM contacts');
+    const rows = await db.query('SELECT * FROM contacts ORDER BY name ASC');
     return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.email === email),
-    ));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts where email = $1', [email]);
+    return row;
   }
 
   delete(id) {
