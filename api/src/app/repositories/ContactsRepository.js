006b4b084a750feddeca5f42c8b18b870866ab44
Pedro Germano <pedrogermano232@gmail.com>
2023-06-23 14:03:26 -0300

    update

--- api/src/app/repositories/ContactsRepository.js
+++ api/src/app/repositories/ContactsRepository.js
@@ -20,8 +20,9 @@ let contacts = [
 ];
 
 class ContactsRepository {
-  async findAll() {
-    const rows = await db.query('SELECT * FROM contacts ORDER BY name ASC');
+  async findAll(orderBy = 'ASC') {
+    const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+    const rows = await db.query(`SELECT * FROM contacts ORDER BY name  ${direction}`);
     return rows;
   }
 
@@ -33,13 +34,6 @@ class ContactsRepository {
   async findByEmail(email) {
     const [row] = await db.query('SELECT * FROM contacts where email = $1', [email]);
     return row;
-  }
-
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
   }
 
   async create({
@@ -55,23 +49,22 @@ class ContactsRepository {
     return row;
   }
 
-  update(id, {
+  async update(id, {
     name, email, phone, category_id,
   }) {
+    // eslint-disable-next-line quotes
+    const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+    `, [name, email, phone, category_id, id]);
+    return row;
+  }
+
+  delete(id) {
     return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
-
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updatedContact : contact
-      ));
-
-      resolve(updatedContact);
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
