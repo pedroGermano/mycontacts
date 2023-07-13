eb32690eed9fe78c79133e88af59222e89b55e54
Pedro Germano <pedrogermano232@gmail.com>
2023-07-13 17:20:01 -0300

    okok teste

--- api/src/index.js
+++ api/src/index.js
@@ -4,8 +4,9 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-// app.use(express.json());
 
 app.use(routes);
+// app.use(routes);
+// app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server stardet at http://localhost:3000'));
