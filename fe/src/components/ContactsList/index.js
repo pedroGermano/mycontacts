06cca2f0a355ff1fa5d12aefd926ad8d2cd8d71a
Pedro Germano <pedrogermano232@gmail.com>
2023-07-27 23:13:41 -0300

    donecard

--- fe/src/components/ContactsList/index.js
+++ fe/src/components/ContactsList/index.js
@@ -41,6 +41,46 @@ export default function ContactsList() {
           </button>
         </div>
       </Card>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Pedro Germano</strong>
+            <small>instagram</small>
+          </div>
+          <span>pedrogermano232@gmail.com</span>
+          <span>(85) 99999-9999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="icon edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="icon trash" />
+          </button>
+        </div>
+      </Card>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Pedro Germano</strong>
+            <small>instagram</small>
+          </div>
+          <span>pedrogermano232@gmail.com</span>
+          <span>(85) 99999-9999</span>
+        </div>
+
+        <div className="actions">
+          <a href="/">
+            <img src={edit} alt="icon edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="icon trash" />
+          </button>
+        </div>
+      </Card>
     </Container>
   );
 }
--- fe/src/components/ContactsList/styles.js
+++ fe/src/components/ContactsList/styles.js
@@ -58,6 +58,10 @@ export const Card = styled.div`
   align-items: center;
   justify-content: space-between;
 
+   & + & {
+    margin-top: 16px;
+   }
+
   .info{
     .contact-name{
       display: flex;
@@ -77,6 +81,18 @@ export const Card = styled.div`
     span{
       display: block;
       font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray[200]}
+    }
+  }
+
+  .actions{
+    display: flex;
+    align-items: center;
+
+    button{
+      background: transparent;
+      border: none;
+      margin-left: 8px;
     }
   }
 `;
