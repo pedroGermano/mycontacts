5dd91c504895a3a85d7e9c8ccd7303a12136fdd4
Pedro Germano <pedrogermano232@gmail.com>
2023-07-27 23:07:23 -0300

    cardcomponent

--- fe/src/components/ContactsList/styles.js
+++ fe/src/components/ContactsList/styles.js
@@ -9,12 +9,11 @@ export const Header = styled.header`
   align-items: center;
   justify-content: space-between;
 
-  strong{
-    color: #222;
+  strong {
     font-size: 24px;
   }
 
-  a{
+  a {
     color: ${({ theme }) => theme.colors.primary.main};
     text-decoration: none;
     font-weight: bold;
@@ -23,9 +22,61 @@ export const Header = styled.header`
     border-radius: 4px;
     transition: all 0.2s ease-in;
 
-    &:hover{
+    &:hover {
       background: ${({ theme }) => theme.colors.primary.main};
       color: #fff;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-right: 8px;
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.primary.main};
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  padding: 16px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .info{
+    .contact-name{
+      display: flex;
+      align-items: center;
+
+      small{
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        text-transform: uppercase;
+        font-weight: bold;
+        padding: 4px;
+        border-radius: 4px;
+        margin-left: 8px;
+      }
+    }
+
+    span{
+      display: block;
+      font-size: 14px;
     }
   }
 `;
