dfb17f79608b3cca5ecbc5c61033e7434128bff8
Pedro Germano <pedrogermano232@gmail.com>
2023-08-03 21:13:15 -0300

    page header props

--- fe/src/pages/EditContact/index.js
+++ fe/src/pages/EditContact/index.js
@@ -1,3 +1,9 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function EditContact() {
-  return 'EditContact';
+  return (
+    <PageHeader
+      title="Editar Pedro Germano"
+    />
+  );
 }
