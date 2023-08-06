5b719d1b832d94c9f260673610fd1919ac7f19f8
Pedro Germano <pedrogermano232@gmail.com>
2023-08-06 18:11:53 -0300

    input start

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,9 +1,16 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/input';
 
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+      <Input
+        type="text"
+        placeholder="Nome"
+      />
+    </>
   );
 }
