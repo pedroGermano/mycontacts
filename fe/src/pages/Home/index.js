21bcf8d0c09bb88f3483af96a5f6c6cebaf4f878
Pedro Germano <pedrogermano232@gmail.com>
2023-08-18 21:33:35 -0300

    loader css

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -7,12 +7,12 @@ import {
 import arrow from '../../assets/img/icons/arrow.svg';
 import edit from '../../assets/img/icons/edit.svg';
 import trash from '../../assets/img/icons/trash.svg';
-import Modal from '../../components/Modal';
+import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
     <Container>
-      <Modal danger />
+      <Loader />
 
       <InputSearchContainer>
         <input
