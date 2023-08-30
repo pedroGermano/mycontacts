53cc17dd5e4a88697686f4b536f9617a77c2ef7f
Pedro Germano <pedrogermano232@gmail.com>
2023-08-29 21:45:24 -0300

    ok

--- fe/src/components/Input.js
+++ fe/src/components/Input.js
@@ -20,6 +20,8 @@ export default styled.input`
 
   ${({ theme, error }) => error && css`
     color: ${theme.colors.danger.main};
+    color: ${theme.colors.danger.main};
+    border-color: ${theme.colors.danger.main} !important;
     border-color: ${theme.colors.danger.main} !important;
   `}
 `;
